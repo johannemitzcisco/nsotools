4fb7e7e0a69090756bd734e075d274d59cae9772
Johan Nemitz <jnemitz@cisco.com>
2021-01-07 22:24:02 -0800

    Update loaddata.sh

--- loaddata.sh
+++ loaddata.sh
@@ -1,6 +1,16 @@
 #!/bin/sh 
 
-/opt/ncs/current/bin/ncs_cli --noaaa << EOF
+ncsdir=/opt/ncs/current
+confdir=/etc/ncs
+rundir=/var/opt/ncs
+logdir=/var/log/ncs
+
+NCS_CONFIG_DIR=${confdir}
+NCS_RUN_DIR=${rundir}
+NCS_LOG_DIR=${logdir}
+export NCS_CONFIG_DIR NCS_RUN_DIR NCS_LOG_DIR
+
+{$ncsdir}/bin/ncs_cli --noaaa << EOF
 config
 load merge $1
 commit dry-run
